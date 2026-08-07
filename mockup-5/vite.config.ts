461c6eb1aefb5a361b727994678f97060d3a2034
Ricky Harerri <ricky_4567@yahoo.co.in>
2026-08-07 15:05:42 -0600

    Dist setting

--- mockup-5/vite.config.ts
+++ mockup-5/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 
 function figmaAssetResolver() {
@@ -17,8 +18,10 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: './',
   plugins: [
     figmaAssetResolver(),
+    viteSingleFile(),
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
